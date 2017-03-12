72f3feba20b679c6c5efb9a2cc0408b10c3d1434
Salma Kona Gugly <gugly2009@gmail.com>
2017-03-12 09:57:31 +0600

    Update testgesture.ts

--- app/components/gesture/testgesture.ts
+++ app/components/gesture/testgesture.ts
@@ -1,32 +1,41 @@
 import {Component} from '@angular/core';
 import {FormControl, FormGroup,FormBuilder, Validators} from '@angular/forms';
+import {Http,Response, Headers, RequestOptions}from '@angular/http';
+import { FormsModule} from '@angular/forms';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Rx';
+
 @Component({
     selector: 'testgesture',
     templateUrl:'app/components/gesture/testgesture.html'
    
 })
+
 export class TestGestureComponent{
 
         gestureformModel: FormGroup;
            
-        constructor(private gf:FormBuilder) {
+        constructor(private gf:FormBuilder,private http:Http) {
          const fb = new FormBuilder();
             this.gestureformModel = fb.group({
-                'name': [null,Validators.required],
+                'label': [null,Validators.required],
                  //'name': [null, Validators.minLength(3)],
                 'description': [null,Validators.required]
             })
         }
-        /*onSubmit() {
-            if (this.gestureformModel.valid) {
-            console.log(this.gestureformModel.value);
-            }
-        }*/
-        onSubmit(formValue: any, isFormValid: boolean) {
+
+       onSubmit(formValue: any, isFormValid: boolean) {
                 if (isFormValid) {
-                console.log(formValue);
+                    var url = "https://braykion.herokuapp.com/api/gestures";
+                    console.log(formValue);
+                    let headers = new Headers({ 'Content-Type': 'application/json' });
+                    let options = new RequestOptions({ headers: headers });
+                    console.log(this.http);
+                    this.http.post(url, {"label":"This is From AG2","description":"test"}, options);
+                    console.log('-----------COMPLITED--------');
+                    }
                 }
-        }
+ 
 }
 
 
