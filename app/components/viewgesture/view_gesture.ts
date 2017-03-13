4de639f5c4e968b852acbb89d820cb498330c778
Salma Kona Gugly <gugly2009@gmail.com>
2017-03-13 19:18:54 +0600

    Update view_gesture.ts

--- app/components/viewgesture/view_gesture.ts
+++ app/components/viewgesture/view_gesture.ts
@@ -1,11 +1,28 @@
-import {Component} from '@angular/core';
+import {Injectable} from "@angular/core";
+import { Component, OnInit,NgModule } from '@angular/core';
+import {FormControl, FormGroup,FormBuilder, Validators} from '@angular/forms';
+import {Http, Response, Headers, RequestOptions}from '@angular/http';
+import { FormsModule} from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import { GestureService }       from './gesture_service';
 
 @Component({
     selector: 'viewgesture',
-    templateUrl:'app/components/viewgesture/view_gesture.html'
+    templateUrl:'app/components/viewgesture/view_gesture.html',
+     providers: [ GestureService ]
    
 })
-export class ViewGestureComponent{
-
+export class ViewGestureComponent implements OnInit{
+    constructor(private service:GestureService){}
+    private posts:any[] = [];
+    private errorMessage:any = '';
+    getPosts() {
+    this.service.getData()
+        .subscribe(
+            posts => this.posts = posts,
+            error => this.errorMessage = <any>error);
+     }
+    ngOnInit() { this.getPosts(); }
 }
- 
